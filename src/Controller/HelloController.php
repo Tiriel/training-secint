5db34860f495a82c497d2191773baa877028b200
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-11-20 16:29:36 +0100

    Twig ex 3 base

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,17 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
     #[Route('/hello/{name?World}', name: 'app_hello_index', requirements: ['name' => '(?:\pL|[- ]?)+'])]
-    public function index(string $name): JsonResponse
+    public function index(string $name): Response
     {
-        return $this->json([
-            'message' => 'Hello '.$name,
-            'path' => 'src/Controller/HelloController.php',
+        return $this->render('hello/index.html.twig', [
+            'name' => $name
         ]);
     }
 }
